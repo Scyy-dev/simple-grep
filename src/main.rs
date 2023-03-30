b997c5cad30bdad31576a29049025a99ddde2a87
Scyphers <scyphers.dev@gmail.com>
2023-03-31 05:06:12 +1100

    add basic file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // error handling handled later
+    // if args.len() < 3 { panic!("Regex and filename not specified") }
+
+    let regex = &args[1];
+    let file = &args[2];
+
+    let lines = fs::read_to_string(file).expect(format!("Could not read file {}", file).as_str());
+
+    println!("Searching file {} using {}", file, regex);
+    println!("File lines are:\n{}", lines);
 }
