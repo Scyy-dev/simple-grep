14116a3991b48a852e9fb1ff10975be845d0b049
Scyphers <scyphers.dev@gmail.com>
2023-03-31 16:17:34 +1100

    refactor arg parsing

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let (regex, filename) = parse_args(&args);
+
     // error handling handled later
     // if args.len() < 3 { panic!("Regex and filename not specified") }
 
-    let regex = &args[1];
-    let file = &args[2];
+    let lines = fs::read_to_string(filename).expect(format!("Could not read file {}", filename).as_str());
 
-    let lines = fs::read_to_string(file).expect(format!("Could not read file {}", file).as_str());
-
-    println!("Searching file {} using {}", file, regex);
+    println!("Searching file {} using {}", filename, regex);
     println!("File lines are:\n{}", lines);
+}
+
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let regex = &args[1];
+    let filename = &args[2];
+
+    (&regex, &filename)
 }
