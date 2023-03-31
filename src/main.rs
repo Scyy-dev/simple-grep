486f1cd0a0d6c75ad5a5eb265392f679a7bc3ca0
Scyphers <scyphers.dev@gmail.com>
2023-03-31 16:47:56 +1100

    convert arg parsing to use a struct

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,31 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (regex, filename) = parse_args(&args);
+    let args = parse_args(&args);
 
     // error handling handled later
     // if args.len() < 3 { panic!("Regex and filename not specified") }
 
-    let lines = fs::read_to_string(filename).expect(format!("Could not read file {}", filename).as_str());
+    dbg!(&args);
 
-    println!("Searching file {} using {}", filename, regex);
+    let lines = fs::read_to_string(&args.file_path).expect(format!("Could not read file {}", &args.file_path).as_str());
+
+    println!("Searching file {} using {}", &args.query, &args.file_path);
     println!("File lines are:\n{}", lines);
 }
 
-fn parse_args(args: &[String]) -> (&str, &str) {
-    let regex = &args[1];
-    let filename = &args[2];
+#[derive(Debug, PartialEq, Clone)]
+struct Args {
+    query: String,
+    file_path: String
+}
 
-    (&regex, &filename)
+impl Args {
+    fn new(regex: &String, file_path: &String) -> Args {
+        Args { query: regex.clone(), file_path: file_path.clone() }
+    }
+}
+
+fn parse_args(args: &[String]) -> Args {
+    Args::new(&args[1], &args[2])
 }
