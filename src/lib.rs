c338049700ccaf04e503b467ce5da60bd2623255
Scyphers <scyphers.dev@gmail.com>
2023-03-31 18:06:14 +1100

    Reimplement search using functional iterators

--- src/lib.rs
+++ src/lib.rs
@@ -23,15 +23,7 @@ pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-
-    results
+    Vec::from_iter(contents.lines().filter(|line| line.contains(query)))
 }
 
 #[cfg(test)]
